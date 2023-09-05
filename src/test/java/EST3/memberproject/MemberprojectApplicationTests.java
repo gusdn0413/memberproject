84a7a6d577e999f479cd5542de58ab33dea7f64b
gusdn0413 <141606385+gusdn0413@users.noreply.github.com>
2023-09-06 00:11:53 +0900

    Initial commit

---
+++ src/test/java/EST3/memberproject/MemberprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package EST3.memberproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
